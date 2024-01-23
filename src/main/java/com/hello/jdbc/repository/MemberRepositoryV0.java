8abe9b3c333af86c73444df1c2ea9cbb0e27d544
Juice <3221ju@gmail.com>
2024-01-23 18:31:05 +0900

    feat: JDBC Read Update Delete

--- src/main/java/com/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/com/hello/jdbc/repository/MemberRepositoryV0.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import com.hello.jdbc.connection.DBConnectionUtil;
 import com.hello.jdbc.domain.Member;
@@ -12,6 +13,79 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class MemberRepositoryV0 {
+
+	public Member findById(String memberId) throws SQLException {
+		String sql = "SELECT * FROM member WHERE MEMBER_ID = ?";
+
+		Connection con = null;
+		PreparedStatement psmt = null;
+		ResultSet rs = null;
+
+		try {
+			con = getConnection();
+			psmt = con.prepareStatement(sql);
+			psmt.setString(1, memberId);
+
+			rs = psmt.executeQuery();
+			if (rs.next()) {
+				Member member = new Member();
+				member.setMemberId(rs.getString("member_id"));
+				member.setMoney(rs.getInt("money"));
+				return member;
+			} else {
+				throw new NoSuchElementException("member not found memberId=" + memberId);
+			}
+		} catch (SQLException e) {
+			log.error("멤버 조회 에러", e);
+			throw e;
+		} finally {
+			close(con, psmt, rs);
+		}
+	}
+
+	public void update(String memberId, int money) throws SQLException {
+		String sql = "UPDATE member SET money=? WHERE member_id = ?";
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			con = getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, money);
+			pstmt.setString(2, memberId);
+			int resultSize = pstmt.executeUpdate();
+			log.info("resultSize={}", resultSize);
+		} catch (SQLException e) {
+			log.error("DB Error", e);
+			throw e;
+		} finally {
+			// 쿼리 실행 후 리소스 정리
+			close(con, pstmt, null);
+		}
+
+	}
+
+	public void delete(String memberId) throws SQLException {
+		String sql = "DELETE FROM member WHERE member_id = ?";
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			con = getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, memberId);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			log.error("DB Error", e);
+			throw e;
+		} finally {
+			// 쿼리 실행 후 리소스 정리
+			close(con, pstmt, null);
+		}
+
+	}
 
 	public Member save(Member member) throws SQLException {
 		String sql = "INSERT INTO member(member_id, money) VALUES (?, ?)";
--- src/test/java/com/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/com/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,18 +1,77 @@
 package com.hello.jdbc.repository;
 
-import java.sql.SQLException;
+import static org.assertj.core.api.Assertions.*;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.hello.jdbc.connection.DBConnectionUtil;
 import com.hello.jdbc.domain.Member;
 
-class MemberRepositoryV0Test {
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+class MemberRepositoryV0Test {
 	private final MemberRepositoryV0 repository = new MemberRepositoryV0();
+
+	@BeforeEach()
+	void cleatDB() throws SQLException {
+		Connection connection = DBConnectionUtil.getConnection();
+		PreparedStatement psmt = connection.prepareStatement("DELETE FROM member");
+
+		psmt.executeUpdate();
+
+		psmt.close();
+		connection.close();
+	}
 
 	@Test
 	void save() throws SQLException {
-		Member member = new Member("memberV0", 10_000);
+		// given
+		String memberId = "memberV0";
+		Member member = new Member(memberId, 10_000);
+
+		// when
 		repository.save(member);
+
+		// then
+		Member findeMember = repository.findById(memberId);
+		assertThat(findeMember).isEqualTo(findeMember);
+	}
+
+	@Test
+	void update() throws SQLException {
+		// given
+		String memberId = "memberV0";
+		Member member = new Member(memberId, 10_000);
+		repository.save(member);
+
+		// when
+		int money = 80_000;
+		repository.update(memberId, money);
+
+		// then
+		Member findeMember = repository.findById(memberId);
+		assertThat(findeMember.getMoney()).isEqualTo(money);
+	}
+
+	@Test
+	void delete() throws SQLException {
+		// given
+		String memberId = "memberV0";
+		Member member = new Member(memberId, 10_000);
+		repository.save(member);
+
+		// when
+		repository.delete(memberId);
+
+		// then
+		assertThatThrownBy(() -> repository.findById(memberId))
+			.isInstanceOf(NoSuchElementException.class);
 	}
 }
