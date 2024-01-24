d67b2d0b61028fc47b72973674ac96ef3509a980
Juice <3221ju@gmail.com>
2024-01-24 21:33:24 +0900

    feat: 예외처리

---
+++ src/test/java/com/hello/jdbc/basic/CheckedTest.java
@@ -0,0 +1,8 @@
+package com.hello.jdbc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CheckedTest {
+
+}
---
+++ src/test/java/com/hello/jdbc/basic/CheckedTestTest.java
@@ -0,0 +1,52 @@
+package com.hello.jdbc.basic;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class CheckedTestTest {
+
+	@Test
+	void checked_catch() {
+		Service service = new Service();
+		service.callCatch();
+	}
+
+	@Test
+	void checked_thrown() {
+		Service service = new Service();
+		assertThatThrownBy(() -> service.callThrow()).isInstanceOf(MyCheckedException.class);
+	}
+
+	static class Service {
+
+		Repository repository = new Repository();
+
+		public void callCatch() {
+			try {
+				repository.call();
+			} catch (MyCheckedException e) {
+				log.info("예외 처리, message={}", e.getMessage(), e);
+			}
+		}
+
+		public void callThrow() throws MyCheckedException {
+			repository.call();
+		}
+	}
+
+	static class Repository {
+		public void call() throws com.hello.jdbc.basic.CheckedTestTest.MyCheckedException {
+			throw new MyCheckedException("ex");
+		}
+	}
+
+	static class MyCheckedException extends Exception {
+		public MyCheckedException(String message) {
+			super(message);
+		}
+	}
+}
